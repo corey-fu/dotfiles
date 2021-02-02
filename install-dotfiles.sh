0b499e6f606551dae852e7bf615c93981a358d39
coreyfu <c12378978@gmail.com>
2021-02-03 01:24:12 +0800

    modified

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -5,5 +5,20 @@ mv ~/.bashrc ~/.bashrc.backup
 cp config/bash/.bashrc ~/
 
 # Copy configs
-rsync -av config/ ~/.config/
+cp -r config ~/.config
 rm -rf ~/.config/bash
+
+# Copy scripts
+cp -r script ~/.script
+
+# Copy openbox themes
+cp -r themes ~/.themes
+
+# Copy bin
+sudo cp -r bin/* /usr/local/bin/
+# Copy icons for tint2
+sudo cp -r config/tint2/icons /usr/local/share/
+
+# Install powerline fonts
+git clone https://github.com/powerline/fonts/ ~/fonts
+sh ~/fonts/install.sh
